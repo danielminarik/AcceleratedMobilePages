ed1fec4631645ff80d8c21936da071b6fed92ae3
danielminarik <daniel.minarik@gmail.com>
2017-05-15 14:58:25 +0200

    moved from Subversion

---
+++ AcceleratedMobilePages/AmpFilterEditExtender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CMS.SiteProvider;
+using CMS;
+using CMS.Base.Web.UI;
+using CMS.PortalEngine.Web.UI;
+
+[assembly: RegisterCustomClass("AmpFilterEditExtender", typeof(AcceleratedMobilePages.AmpFilterEditExtender))]
+
+namespace AcceleratedMobilePages
+{
+    class AmpFilterEditExtender : ControlExtender<UIForm>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public override void OnInit()
+        {
+            Control.OnBeforeSave += Control_OnBeforeSave;
+        }
+
+
+        /// <summary>
+        /// Sets the site ID before save
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event arguments</param>
+        private void Control_OnBeforeSave(object sender, EventArgs e)
+        {
+            AmpFilterInfo elementInfo = (AmpFilterInfo) Control.EditedObject;
+            elementInfo.SiteID = SiteContext.CurrentSiteID;
+            if (elementInfo.UseDefaultStylesheet)
+            {
+                elementInfo.StylesheetID = 0;
+            }
+        }
+    }
+}
---
+++ AcceleratedMobilePages/CustomMacroMethods.cs
@@ -0,0 +1,17 @@
+﻿using CMS.DataEngine;
+using CMS.DocumentEngine;
+using CMS.Helpers;
+using CMS.MacroEngine;
+using CMS.SiteProvider;
+
+namespace AcceleratedMobilePages
+{
+    public class CustomMacroMethods : MacroMethodContainer
+    {
+        [MacroMethod(typeof(bool), "Returns true if the current page is AMP page.", 1)]
+        public static object IsAmpPage(EvaluationContext context, params object[] parameters)
+        {
+            return (AcceleratedMobilePages.CheckStateHelper.CheckFilterState() == Constants.ENABLED_AND_ACTIVE);
+        }
+    }
+}
